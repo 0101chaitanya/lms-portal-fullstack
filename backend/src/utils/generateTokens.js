66cb9310b26448097224766307969ef981be309a
0101chaitanya <0101chaitanya@gmail.com>
2026-07-01 22:14:20 +0530

    fix: update cookie sameSite attribute to support cross-site requests in production

--- backend/src/utils/generateTokens.js
+++ backend/src/utils/generateTokens.js
@@ -15,7 +15,7 @@ export const generateRefreshToken = (res, id) => {
     res.cookie('jwt', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', 
-        sameSite: 'strict', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', 
         maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
